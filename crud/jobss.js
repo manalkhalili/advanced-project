e2e1bfc043bd2a64241fc7c83c596eef26ab4951
marah126 <s11923808@stu.najah.edu~>
2023-05-22 02:44:57 +0300

    read property

--- crud/jobss.js
+++ crud/jobss.js
@@ -16,6 +16,19 @@ router.post('/create',(req,res,next)=>{
             return res.status(500).json(err);
         }
     });
+});
+
+//2- read //////////////////////////////////////////////////////////////////
+router.get('/read',(req,res,next)=>{
+    var quary = "select *from job";
+    connection.query(quary,(err,results)=>{
+        if(!err){
+            return res.status(200).json(results);
+        }
+        else{
+            return res.status(500).json(err);
+        }
+    })
 })
 
 module.exports=router;
