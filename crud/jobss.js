7001eeb81ba24abfd7afab93065391dedae333d2
marah126 <s11923808@stu.najah.edu~>
2023-05-22 03:54:18 +0300

    .

--- crud/jobss.js
+++ crud/jobss.js
@@ -7,7 +7,7 @@ const router = express.Router();
 //1- creat//////////
 router.post('/create',(req,res,next)=>{
     let job=req.body;
-    query="insert into job (title,location,terms,salary,requirments,aboutus,abouttherole,contactus) values(?,?,?,?,?,?,?,?,?)";
+    query="insert into job (title,location,terms,salary,requirments,aboutus,abouttherole,contactus) values(?,?,?,?,?,?,?,?)";
     connection.query(query,[job.title,job.location,job.terms,job.salary,job.requirments,job.aboutus,job.abouttherole,job.contactus],(err,results)=>{
         if(!err){
             return res.status(200).json({massage: "job is inserted successfully"});
