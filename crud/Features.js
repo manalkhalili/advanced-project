9efa8b8994f1cf547979226f8202a20d04ea7855
manalkhalili <khalilim387@gmail.com>
2023-05-22 03:54:30 +0300

    hi

---
+++ crud/Features.js
@@ -0,0 +1,35 @@
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/jobs', (req, res) => {
+    const query = 'SELECT * FROM jobs';
+    pool.query(query, (error, results) => {
+      if (error) {
+        console.error('Error executing MySQL query:', error);
+        res.status(500).json({ error: 'Internal server error' });
+      } else {
+        res.json(results);
+      }
+    });
+  });
+  
+  /*app.post('/jobs', (req, res) => {
+    const { title, abouttherole, requirements, salary } = req.body;
+    const query = `INSERT INTO jobs (title, abouttherole, requirements, salary) VALUES (?, ?, ?, ?)`;
+    const values = [title, abouttherole, requirements, salary];
+    pool.query(query, values, (error, result) => {
+      if (error) {
+        console.error('Error executing MySQL query:', error);
+        res.status(500).json({ error: 'Internal server error' });
+      } else {
+        res.status(201).json({ id: result.insertId, ...req.body });
+      }
+    });
+  });*/
+  
+  // Start the server
+  app.listen(4002, () => {
+    console.log('Server started on port 4002');
+  });
+
+
