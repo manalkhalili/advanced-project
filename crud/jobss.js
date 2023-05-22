fe591f1f56cef77213183aa98f3fd3ede4d4edf6
manalkhalili <khalilim387@gmail.com>
2023-05-22 12:29:49 +0300

    g

--- crud/jobss.js
+++ crud/jobss.js
@@ -66,3 +66,15 @@ router.delete('/delete/:id',(req,res,next)=>{
     });
 });
 module.exports=router;
+/////////////////////list//////////////
+router.get('/list',(req,res,next)=>{
+    var quary = "select *from job";
+    connection.query(quary,(err,results)=>{
+        if(!err){
+            return res.status(200).json(results);
+        }
+        else{
+            return res.status(500).json(err);
+        }
+    });
+});
