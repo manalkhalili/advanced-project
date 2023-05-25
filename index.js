38a7926edecf03eb2a333b9ded329d0d3e6bac4a
manalkhalili <khalilim387@gmail.com>
2023-05-25 08:35:53 +0300

    d

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express =require('express');
 const connection = require("./connection");
 const jobRoute=require('./crud/jobss');
 
+
 const app=express();
 app.use(express.urlencoded({extended : true}));
 app.use(express.json());
