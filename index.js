e2790e29744396339a33d018a263acb8443faa15
marah126 <s11923808@stu.najah.edu~>
2023-05-25 03:29:08 +0300

    edit

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express =require('express');
 const connection = require("./connection");
 const jobRoute=require('./crud/jobss');
-const fileUpload=require('express-fileupload');
+
 
 
 const app=express();
 
 app.use(express.urlencoded({extended : true}));
 app.use(express.json());
-app.use(fileUpload());
+
 app.use('/jobs',jobRoute);
 
 
